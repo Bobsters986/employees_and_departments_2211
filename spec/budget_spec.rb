1e428dc52d6dd08a7b7f52f9dca0f49c24b8a74f
Bobby Luly <bobbyluly@gmail.com>
2023-01-13 10:16:52 -0700

    Test&Feat: create test to #add_department and create method to pass test

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -4,8 +4,10 @@ require './lib/budget'
 
 RSpec.describe Budget do
   let(:customer_service) { Department.new("Customer Service") }
+  let(:h_r) { Department.new("Human Resources") }
   let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
   let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "$90000"}) }
+  let(:jill) { Employee.new({name: "Jill Stahley", age: "33", salary: "$95000"}) }
   let(:budget) { Budget.new("State of Colorado", 2022) }
 
    describe '#initialize' do
@@ -19,4 +21,21 @@ RSpec.describe Budget do
       expect(budget.departments).to eq([])
     end
   end
+
+  describe 'budget can add departments' do
+    before do
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+      h_r.hire(jill)
+    end
+
+    it 'can #add_department' do
+      budget.add_department(customer_service)
+      budget.add_department(h_r)
+
+      expect(budget.departments).to eq([customer_service, h_r])
+    end
+  end
+
+  
 end
