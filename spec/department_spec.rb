fcde8a50b19a1a6f2d5ded9dad5abcd496f4d847
Bobby Luly <bobbyluly@gmail.com>
2023-01-13 09:27:00 -0700

    Test&Feat: add test to calculate department expense and add expense

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -17,12 +17,21 @@ RSpec.describe Department do
     end
   end
 
-  describe '#hire_employees to add to employees array' do
+  describe '#hire_employees and calculate expenses' do
     it 'can #hire_employees' do
       customer_service.hire(bobbi)
       customer_service.hire(aaron)
 
       expect(customer_service.employees).to eq([bobbi, aaron])
+    end
+
+    it 'starts with no #expenses can add #expense' do
+      expect(customer_service.expenses).to eq(0)
+
+      customer_service.expense(100)
+      customer_service.expense(25)
+
+      expect(customer_service.expenses).to eq(125)
     end
   end
 end
