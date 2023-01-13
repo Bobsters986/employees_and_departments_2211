0896b07b205a465a82965e426b3c9ac2f4917693
Bobby Luly <bobbyluly@gmail.com>
2023-01-13 10:05:46 -0700

    Create budget spec and lib, create initialize tests

---
+++ spec/budget_spec.rb
@@ -0,0 +1,20 @@
+require './lib/employee'
+require './lib/department'
+
+RSpec.describe Budget do
+  let(:customer_service) { Department.new("Customer Service") }
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
+  let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "$90000"}) }
+  let(:budget) { Budget.new("State of Colorado", 2022) }
+
+   describe '#initialize' do
+    it 'exists' do
+      expect(budget).to be_a(Budget)
+    end
+
+    it 'has attributes' do
+      expect(budget.name).to eq("State of Colorado")
+      expect(budget.year).to eq(2022)
+    end
+  end
+end
