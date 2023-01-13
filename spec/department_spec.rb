62c91febe72f2a99d40639355a86721515901e3a
Bobby Luly <bobbyluly@gmail.com>
2023-01-13 09:21:39 -0700

    Test: create test for adding employees and create method to pass

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -3,6 +3,8 @@ require './lib/department'
 
 RSpec.describe Department do
   let(:customer_service) { Department.new("Customer Service") }
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
+  let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "$90000"}) }
 
    describe '#initialize' do
     it 'exists' do
@@ -12,6 +14,15 @@ RSpec.describe Department do
     it 'has attributes' do
       expect(customer_service.name).to eq("Customer Service")
       expect(customer_service.employees).to eq([])
+    end
+  end
+
+  describe '#hire_employees to add to employees array' do
+    it 'can #hire_employees' do
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+
+      expect(customer_service.employees).to eq([bobbi, aaron])
     end
   end
 end
